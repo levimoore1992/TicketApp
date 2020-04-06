800bdfd8f1e8ed1990ec2e18d57a91be85247437
levimoore1992 <levi.moore@exxonmobil.com>
2020-04-05 20:27:58 -0500

    frontend will post to backend

--- frontend/src/app/post-listing/post-listing.component.ts
+++ frontend/src/app/post-listing/post-listing.component.ts
@@ -11,13 +11,15 @@ export class PostListingComponent implements OnInit {
   postForm: FormGroup;
   public source;
   data: any;
+  allData;
+  private eventId: any;
 
   constructor(private fb: FormBuilder, private postService: PostListingService) {
           this.postService.getEvents().subscribe(res => {
       // @ts-ignore
         this.source = res.map(item => item.name);
         this.data = this.source.slice();
-
+        this.allData = res;
     });
   }
 
@@ -29,19 +31,26 @@ export class PostListingComponent implements OnInit {
     });
   }
     handleFilter(value: string) {
-            this.data = this.source.filter((s) => s.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+      this.data = this.source.filter((s) => s.toLowerCase().indexOf(value.toLowerCase()) !== -1);
   }
 
   submitForm() {
+
+    const thisEvent = this.postForm.get('event').value;
+    for (const event of this.allData) {
+      if (event.name === thisEvent) {
+        this.eventId = event.id;
+        break;
+      }
+    }
     const payload = {
-    event: this.postForm.get('event').value,
+    event: this.eventId,
     price: this.postForm.get('price').value
     };
-
-    this.postService.createPost(payload).subscribe(res =>{
+    this.postService.createPost(payload).subscribe(res => {
       console.log(res);
       }
-    )
+    );
   }
 
 }
