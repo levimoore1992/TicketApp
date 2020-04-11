115fa239fed105b7049d53353769acd4c6bf744c
levimoore1992 <levi.moore@exxonmobil.com>
2020-04-11 16:28:36 -0500

    Revert "added one to one field"
    This reverts commit 71b6caaa5cac8696a815af4e9b6d77af83a711f2.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,27 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 # Create your models here.
-
-class Account(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    profile_picture = models.ImageField(null=True)
-
-    def __repr__(self):
-        return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Account.objects.create(user=instance)
-    instance.profile.save()
-
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.account.save()
