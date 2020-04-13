f1723d90fdfd80497248a610e7930e09ca913f89
levimoore1992 <levi.moore@exxonmobil.com>
2020-04-12 22:24:30 -0500

    added a status to listings

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 
 
 class Listing(models.Model):
+    STATUS = [
+        ('NOT SENT', 'NOT SENT'),
+        ('SENT', 'SENT'),
+        ('DELIVERED','DELIVERED')
+    ]
     event = models.ForeignKey(Event, on_delete=models.CASCADE, to_field='name')
     seller = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     price = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUS, default='NOT SENT')
 
     def __str__(self):
         return f'{self.seller}'
